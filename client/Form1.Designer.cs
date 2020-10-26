75e640e8c6126bc61cb63e427eec35fcee8aa2b7
Oktay Agca <58532036+oktayagca@users.noreply.github.com>
2020-10-26 20:23:41 +0300

    Add files via upload

---
+++ client/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace CLİENT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.MessagetextBox = new System.Windows.Forms.TextBox();
+            this.ChatScreentextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.ClientPorttextBox = new System.Windows.Forms.TextBox();
+            this.ClientIPtextBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.lblzaman = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(474, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 37);
+            this.button3.TabIndex = 23;
+            this.button3.Text = "GÖNDER";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MessagetextBox
+            // 
+            this.MessagetextBox.Location = new System.Drawing.Point(52, 224);
+            this.MessagetextBox.Multiline = true;
+            this.MessagetextBox.Name = "MessagetextBox";
+            this.MessagetextBox.Size = new System.Drawing.Size(388, 37);
+            this.MessagetextBox.TabIndex = 22;
+            // 
+            // ChatScreentextBox
+            // 
+            this.ChatScreentextBox.Location = new System.Drawing.Point(52, 69);
+            this.ChatScreentextBox.Multiline = true;
+            this.ChatScreentextBox.Name = "ChatScreentextBox";
+            this.ChatScreentextBox.Size = new System.Drawing.Size(497, 135);
+            this.ChatScreentextBox.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 1);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "CLİENT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(224, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "PORT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "İP";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(474, 28);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 17;
+            this.ConnectButton.Text = "BAĞLAN";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // ClientPorttextBox
+            // 
+            this.ClientPorttextBox.Location = new System.Drawing.Point(267, 31);
+            this.ClientPorttextBox.Name = "ClientPorttextBox";
+            this.ClientPorttextBox.Size = new System.Drawing.Size(173, 20);
+            this.ClientPorttextBox.TabIndex = 16;
+            // 
+            // ClientIPtextBox
+            // 
+            this.ClientIPtextBox.Location = new System.Drawing.Point(52, 31);
+            this.ClientIPtextBox.Name = "ClientIPtextBox";
+            this.ClientIPtextBox.Size = new System.Drawing.Size(166, 20);
+            this.ClientIPtextBox.TabIndex = 15;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // lblzaman
+            // 
+            this.lblzaman.AutoSize = true;
+            this.lblzaman.Location = new System.Drawing.Point(556, 37);
+            this.lblzaman.Name = "lblzaman";
+            this.lblzaman.Size = new System.Drawing.Size(0, 13);
+            this.lblzaman.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 311);
+            this.Controls.Add(this.lblzaman);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.MessagetextBox);
+            this.Controls.Add(this.ChatScreentextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.ClientPorttextBox);
+            this.Controls.Add(this.ClientIPtextBox);
+            this.Name = "Form1";
+            this.Text = "CLİENT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox MessagetextBox;
+        private System.Windows.Forms.TextBox ChatScreentextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox ClientPorttextBox;
+        private System.Windows.Forms.TextBox ClientIPtextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label lblzaman;
+    }
+}
+
---
+++ client/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace CLİENT
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string recieve=" ";
+        public string TextToSend;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient();
+            IPEndPoint ıPEnd = new IPEndPoint(IPAddress.Parse(ClientIPtextBox.Text), int.Parse(ClientPorttextBox.Text));
+            label4.Visible = false;
+            label5.Visible = false;
+            ClientIPtextBox.Visible = false;
+            ClientPorttextBox.Visible = false;
+            ConnectButton.Visible = false;
+
+            try
+            {
+                client.Connect(ıPEnd);
+                if (client.Connected)
+                {
+                    ChatScreentextBox.AppendText("Connected to Server" + "\n");
+                    STR = new StreamReader(client.GetStream());
+                    STW = new StreamWriter(client.GetStream());
+                    STW.AutoFlush = true;
+                    backgroundWorker1.RunWorkerAsync();
+                    backgroundWorker2.WorkerSupportsCancellation = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            while (client.Connected)
+            {
+                try
+                {
+
+                    recieve = STR.ReadLine();
+                    this.ChatScreentextBox.Invoke(new MethodInvoker(delegate ()
+                    {
+                        ChatScreentextBox.AppendText("SORU:\n " + recieve + "\n");
+                    }));
+                    if (recieve == "ELENDİNİZ")
+                    {
+                        MessageBox.Show("elendiniz");
+                        
+                        client.Close();
+                        this.Close();
+                        Application.Exit();
+                    }
+                    recieve = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (client.Connected)
+            {
+                STW.WriteLine(TextToSend);
+                this.ChatScreentextBox.Invoke(new MethodInvoker(delegate ()
+                {
+                    ChatScreentextBox.AppendText("CEVAP:\n" + TextToSend + "\n");
+                }));
+            }
+            else
+            {
+                MessageBox.Show("Sending Failed");
+            }
+            backgroundWorker2.CancelAsync();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (recieve != " ")
+            {
+                if (MessagetextBox.Text != "")
+                {
+
+                    TextToSend = MessagetextBox.Text;
+                    backgroundWorker2.RunWorkerAsync();
+                    recieve = " ";
+                }
+                MessagetextBox.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Soru gelmeden cevap veremezsiniz");
+            }
+        }
+
+
+
+    }
+}
